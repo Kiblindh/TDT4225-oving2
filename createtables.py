b5bfa65a8ad475ee7183560b19e9baa270f7b355
kiblindh <kiblindh@ntnu.no>
2025-09-28 13:31:05 +0200

    Started working on inserting data

--- createtables.py
+++ createtables.py
@@ -1,6 +1,7 @@
 
 from DbConnector import DbConnector
 from tabulate import tabulate
+import pandas as pd
 
 class CreateTables:
     def __init__(self):
@@ -10,9 +11,12 @@ class CreateTables:
 
     def create_trip_table(self, table_name):
         query = """CREATE TABLE IF NOT EXISTS %s (
-                tripId INT PRIMARY KEY,
+                tripId BIGINT PRIMARY KEY,
                 taxiId VARCHAR(50) NOT NULL,
-                startTime TEXT NOT NULL)
+                startTime TEXT NOT NULL,
+                dayType CHAR(1) NOT NULL,
+                missingData BOOLEAN NOT NULL
+                )
         """
         self.cursor.execute(query % table_name)
         self.db_connection.commit()
@@ -34,8 +38,8 @@ class CreateTables:
                 tripId INTEGER NOT NULL,
                 pointId INTEGER NOT NULL,
                 idx INTEGER NOT NULL,
-                FOREIGN KEY (tripId) REFERENCES trip(tripId),
-                FOREIGN KEY (pointId) REFERENCES point(pointId),
+                FOREIGN KEY (tripId) REFERENCES trip(tripId) ON DELETE CASCADE,
+                FOREIGN KEY (pointId) REFERENCES point(pointId) ON DELETE CASCADE,
                 PRIMARY KEY (tripId, idx)
             )
         '''
@@ -73,6 +77,32 @@ class CreateTables:
         self.cursor.execute("SHOW TABLES")
         rows = self.cursor.fetchall()
         print(tabulate(rows, headers=self.cursor.column_names))
+
+    def read_porto_csv(filepath='porto/porto/porto.csv'):
+        """
+        Reads the porto.csv file and returns a pandas DataFrame.
+        """
+        df = pd.read_csv(filepath)
+        return df
+
+    def insert_data(self):
+        df = self.read_porto_csv()
+        for row in df:
+            tripId = row['TRIP_ID'].astype(int)
+            taxiId = row['TAXI_ID'].astype(int)
+            startTime = pd.to_datetime(row['TIMESTAMP'], unit='s')
+            dayType = row['DAY_TYPE'].astype(str)
+            missingData = row['MISSING_DATA'].astype(bool)
+            callerId = row['ORIGIN_CALL'].astype(int) if not pd.isnull(row['ORIGIN_CALL']) else None
+            standId = row['ORIGIN_STAND'].astype(int) if not pd.isnull(row['ORIGIN_STAND']) else None
+            
+
+            tripQuery = """
+            INSERT INTO trip (tripId, taxiId, startTime, dayType, missingData) VALUES (%s, %s, %s, %s, %s)
+            """
+
+            self.cursor.execute(tripQuery, (tripId, taxiId, startTime, dayType, missingData))
+        self.db_connection.commit()
 
 def main():
     program = None
